e6ff73f8139127bdcc72716bc0e4f16d9d1ad8dc
Isra90 <127267228+Isra90@users.noreply.github.com>
2023-12-11 18:47:20 -0500

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -27,52 +27,95 @@ Game::Game(string filename, string play_chopsticks) {
     }
     //read into cards and then put them into the deck
 
-    string line;
+    string line = "";
     //Discard first line
     deckFile >> line >> line;
+    file_deck = new Vector();
+
     while (getline(deckFile, line)) {
-        Card card;
+        Card* card = new Card();
         stringstream ss(line);
-        string element;
+        string element = "";
         int index = 0;
         while (ss >> element) {
             if (index == 0) {
-                card.set_sushiType(element);
+                card->set_sushiType(element);
             } else if (index == 1) {
-                card.set_makiCount(stoi(element));
+                card->set_makiCount(stoi(element));
             }
+            index++;
         }
-        file_deck.addCard(card);
+        index = 0;
+        file_deck->push_back(card);
     }
 
 }
 
 //main game loop
+// playGame
+// Input: None
+// Description: Plays the game
+// Output: None
 void Game::playGame() {
 
     int card_index;
     string play_on;
-
     //play three rounds
-    for(int round = 0; round < ROUNDS; round++){
+    for (int round = 0; round < ROUNDS; round++) {
+
         /*   TODO (Part I): Deal 9 cards to each player          */
-        Deck draw_deck;
+        for (int p = 0; p < PLAYER_COUNT; p++) {
+            Vector* player_deck = new Vector();
+
+            for (int card_count = 0; card_count < CARD_HAND; card_count++) {
+                Card* file_deck_card = file_deck->back();
+                file_deck->pop_back();
+                player_deck->push_back(file_deck_card);
+            }
+            players[p].setPassingHand(player_deck);
+        }
+        
         //select and pass all 27 cards
-        for(int card = 0; card < CARD_HAND; card++) {
-            Card file_deck_card = file_deck.getTopCard();
-            draw_deck.addCard(file_deck_card);
-            cout << "Hello" << endl;
+        for (int card = 0; card < CARD_HAND; card++) {
+            Card* revealed_cards[PLAYER_COUNT];
             for(int player = 0; player < PLAYER_COUNT; player++) {
-                cout << "Hellour" << endl;
                 board.drawBoard(players, player);
+
+                if (players[player].getPassingHand()->size() == 0) break;
 
                 cout << " Player " << player + 1 << ", select a card: ";
                 cin >> card_index;
 
+                if (cin.fail()) {
+                    cout << "INVALID INPUT: No game for you!" << endl;
+                    return;
+                }
+
+                while (card_index < 1 || card_index > 9) {
+                    cout << "     Please enter a valid number between 1 and 9: ";
+                    cin >> card_index;
+                    if (cin.fail()) {
+                        cout << "INVALID INPUT: No game for you!" << endl;
+                        return;
+                    }
+                }
+
                 /*   TODO (Part I): Set aside selected card      */
+                Card* selected = players[player].getPassingHand()->at(card_index - 1);
+                players[player].getPassingHand()->erase(card_index - 1);
+                revealed_cards[player] = selected;
             }
+            
             /*   TODO (Part I): Reveal selected cards            */
+            for(int player = 0; player < PLAYER_COUNT; player++) {
+                players[player].pushRevealedHand(*revealed_cards[player]);
+            }
+            //cout << "made it here" << endl;
             /*   TODO (Part I): Pass deck to the right           */
+            Vector* temp = players[1].getPassingHand();
+            players[1].setPassingHand(players[0].getPassingHand());
+            players[2].setPassingHand(players[1].getPassingHand());
+            players[0].setPassingHand(temp);
         }
         
         break;
@@ -96,8 +139,14 @@ void Game::playGame() {
     /*   TODO (Part II): Call board.drawWinner(players, winner); */
 }
 
+void Game::clean_playGame() {
+
+}
+
 //destructor
 Game::~Game() {
+    delete file_deck;
+    // delete cards?
     /*-----------------------------------------------*/
     /*   TODO (Parts I and II): CLEAN UP THE GAME    */
     /*                                               */
