1aeba37eade8e914215468bd63dcad16048f0e0a
Isra90 <127267228+Isra90@users.noreply.github.com>
2023-12-11 18:51:59 -0500

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -3,25 +3,68 @@
 
 //constructor
 Player::Player() {
-    
+    passing_hand = new Vector();
+    revealed_hand = new Vector();
 }
 
 Player::~Player() {
-    
+    delete passing_hand;
+    delete revealed_hand;
 }
 
+// setPassingHand
+// Input: string passing_hand
+// Description: set passing_hand as string hand
+// Output: None
+void Player::setPassingHand(Vector* hand) {
+    //passing_hand->push_back(&card);
+    passing_hand = hand;
+}
+
+/*void Player::setRevealedHand(Vector* hand) {
+    revealed_hand = hand;
+}*/
+
+/*void Player::pushPassingHand(Card &card) {
+    passing_hand->push_back(&card);
+}*/
+
+// pushRevealedHand
+// Input: card
+// Description: push a card onto the revealed hand vector
+// Output: None
+void Player::pushRevealedHand(Card &card) {
+    revealed_hand->push_back(&card);
+}
+
+// getPassingHand
+// Input: None
+// Description: return the passing hand
+// Output: passing_hand
 Vector* Player::getPassingHand() {
-    return hand;
-}
-Vector* Player::getRevealedCards(){
-    return hand;
+    return passing_hand;
 }
 
+// getRevealedCards
+// Input: None
+// Description: return the revealed
+// Output: revealed_hand
+Vector* Player::getRevealedCards(){
+    return revealed_hand;
+}
+
+// getsScore
+// Input: 
+// Description: 
+// Output: 
 int Player::getScore(){
     return 0;
 }
 
+// getPuddingCount
+// Input:
+// Description: 
+// Output: 
 int Player::getPuddingCount(){
     return 0;
 }
-
